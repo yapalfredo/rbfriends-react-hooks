ea1f80538b7b31443553821a6fd5b76afce8ba81
al <alfredo.yap@stu.bmcc.cuny.edu>
2022-12-28 16:42:25 -0500

    started implementing CardList

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Card from './Card.js';
-import 'tachyons';
+import CardList from './CardList.js'
 import {robots} from './robots';
+import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* with props */}
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+    <CardList robots={robots}/>
   </React.StrictMode>
 );
