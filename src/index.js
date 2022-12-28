044a2a18589af4ef9df0959ba5e7a7fc755fa9e0
al <alfredo.yap@stu.bmcc.cuny.edu>
2022-12-28 02:16:19 -0500

    secong commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import Hello from './Hello.js'
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import Card from './Card.js';
+import 'tachyons';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* with props */}
-    <Hello greeting={'Hello React Ninja'}/>
+    <Card />
+    <Card />
+    <Card />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
