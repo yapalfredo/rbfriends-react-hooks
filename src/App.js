3656981140c517a52834fd313045918d6543cdd6
al <alfredo.yap@stu.bmcc.cuny.edu>
2022-12-28 01:39:37 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
+
+/* Functional Component
 function App() {
   return (
     <div className="App">
@@ -15,11 +18,37 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learning React
         </a>
       </header>
     </div>
   );
 }
+*/
+
+//Class Component
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learning React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
 
 export default App;
