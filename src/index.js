e77403dca03dd940d2fc03cc7bc1e907bef4153e
al <alfredo.yap@stu.bmcc.cuny.edu>
2022-12-28 02:24:50 -0500

    created cards and edited readme

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Card from './Card.js';
 import 'tachyons';
-
+import {robots} from './robots'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* with props */}
-    <Card />
-    <Card />
-    <Card />
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
   </React.StrictMode>
 );
