14a0993065998fc0075b698d62c23573f25c7f9d
al <alfredo.yap@stu.bmcc.cuny.edu>
2022-12-28 21:59:41 -0500

    fixed the folders and cleaned up the codes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import CardList from './CardList.js'
-import SearchBox from './SearchBox.js'
-import Scroll from "./Scroll.js";
+import CardList from '../components/CardList.js'
+import SearchBox from '../components/SearchBox.js'
+import Scroll from "../components/Scroll.js";
 import './App.css'
 
 class App extends Component {
@@ -36,36 +36,35 @@ class App extends Component {
     }
 
     render() {
+        const {robots, searchfield} = this.state;
         //This variable will tracks the searchField state and
         //filter the robots array based on the search entry.
         //This will also be passed to the robots prop instead.
-        const filteredRobots = this.state.robots.filter(robot =>{
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        const filteredRobots = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
 
         //This checks if the robots state is empty
         //if it is, shows loading
         //else pass the robots data from the web to the cardlist
-        if (this.state.robots.length === 0)
-        {
-            return <h1>Loading</h1>
-        }
-        else {
-            return (
-                <div className="tc">
-                    <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
+       
+        //Ternary
+        return !robots.length ?
+            <h1>Loading</h1> :
+        (
+            <div className="tc">
+            <h1 className="f1">RoboFriends</h1>
+            <SearchBox searchChange={this.onSearchChange}/>
 
-                    {/* Wraps the CardList component with Scroll.
-                        What happens automatically, React creates a prop
-                        for Scroll to access its children
-                    */}
-                    <Scroll>
-                        <CardList robots={filteredRobots}/>
-                    </Scroll>
-                </div>
-            );
-        }
+            {/* Wraps the CardList component with Scroll.
+                What happens automatically, React creates a props
+                for Scroll to access its children
+            */}
+            <Scroll>
+                <CardList robots={filteredRobots}/>
+            </Scroll>
+        </div>
+        )
 
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js'
+import App from './containers/App.js'
 import './index.css'
 import 'tachyons';
 
