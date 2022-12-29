e14d09dac7831ebc3847ed788c94f1df656a5a5b
al <alfredo.yap@stu.bmcc.cuny.edu>
2022-12-28 21:14:05 -0500

    implemented componentDidMount and fetch() to get the robots data from a url

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from './CardList.js'
 import SearchBox from './SearchBox.js'
 import {robots} from './robots';
+import './App.css'
 
 class App extends Component {
     //Standard way of initializing STATE in React.
@@ -11,9 +12,15 @@ class App extends Component {
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.cypress.io/users')
+        .then(res => res.json())
+        .then(users => this.setState({robots: users}));
     }
 
     //custom function
@@ -30,13 +37,24 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot =>{
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-        return (
+
+        //This checks if the robots state is empty
+        //if it is, shows loading
+        //else pass the robots data from the web to the cardlist
+        if (this.state.robots.length === 0)
+        {
+            return <h1>Loading</h1>
+        }
+        else {
+            return (
                 <div className="tc">
-                <h1>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
-            </div>
-        );
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
+        }
+
     }
 }
 
