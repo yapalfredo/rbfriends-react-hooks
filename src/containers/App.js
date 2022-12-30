73d94f4093c001e9cc343f22230cb853ade414ad
al <alfredo.yap@stu.bmcc.cuny.edu>
2022-12-30 01:20:34 -0500

    converted old project from class based to function to use Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-//Convert from class to function
+//Convert from class to function (doesn't use "this")
 
 import React, { useState, useEffect } from "react";
 import CardList from '../components/CardList.js'
@@ -9,7 +9,7 @@ import './App.css'
 
 function App() {
 
-    //Hooks
+    //Initializing Hooks needed
     const [robots, setRobots] = useState([])
     const [searchfield, setSearchfield] = useState('')
 
@@ -17,27 +17,35 @@ function App() {
         setSearchfield(event.target.value)
     }
 
-        //This variable will tracks the searchField state and
-        //filter the robots array based on the search entry.
-        //This will also be passed to the robots prop instead.
-        const filteredRobots = robots.filter(robot =>{
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-        })
+    //This variable will tracks the searchField state and
+    //filter the robots array based on the search entry.
+    //This will also be passed to the robots prop instead.
+    const filteredRobots = robots.filter(robot =>{
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    })
 
-        return !robots.length ?
-            <h1>Loading</h1> :
-        (
-            <div className="tc">
-            <h1 className="f1">RoboFriends</h1>
-            <SearchBox searchChange={onSearchChange}/>
+    // The '[]' is the shortcut for componentDidMount()
+    // and also replicates the behavior of it
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.cypress.io/users')
+        .then(res => res.json())
+        .then(users => setRobots(users));
+    },[])
 
-            <Scroll>
-                <ErrorBoundary>
-                    <CardList robots={filteredRobots}/>
-                </ErrorBoundary>
-            </Scroll>
-        </div>
-        )
+    return !robots.length ?
+        <h1>Loading</h1> :
+    (
+        <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={onSearchChange}/>
+
+        <Scroll>
+            <ErrorBoundary>
+                <CardList robots={filteredRobots}/>
+            </ErrorBoundary>
+        </Scroll>
+    </div>
+    )
 }
 
 export default App;
