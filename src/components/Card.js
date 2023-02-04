7516427d604e45dedff418e0dbf725f7aeec90c6
al <alfredo.yap@stu.bmcc.cuny.edu>
2023-02-04 00:35:23 -0500

    change id to name in robohash url

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ const Card = ({ name, email, id }) => {
 
   return (
     <div className="tc bg-light-blue  dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img alt="Robot" src={`https://robohash.org/${id}?200x200`} />
+      <img alt="Robot" src={`https://robohash.org/${name}?200x200`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
